8a1019622c6dfb92e496fb802bb06443181b5218
aditya <kumar.93@iitj.ac.in>
2020-06-15 14:02:06 +0530

    modified database settings for postgres

--- eduHub/settings.py
+++ eduHub/settings.py
@@ -82,8 +82,8 @@ ASGI_APPLICATION = "eduHub.routing.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'eduHub',
     }
 }
 
