7f8f1287ff8992b10a082724018ce80a39477bb8
kumardot93 <52152749+kumardot93@users.noreply.github.com>
2020-06-16 20:39:28 +0530

    Update routing.py

--- eduHub/routing.py
+++ eduHub/routing.py
@@ -3,6 +3,7 @@ from material.api.ws.rouing import websocket_urlpatterns as materials_ws_urls
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
 
+#Entry point for the whole ASGI APP
 application = ProtocolTypeRouter({
     # Empty for now (http->django views is added by default)
     'websocket': AuthMiddlewareStack(  #Author middleware stack for authentication
