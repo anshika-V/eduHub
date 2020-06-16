3e59f72170866c91a8a84b3569af45622628efd6
kumardot93 <52152749+kumardot93@users.noreply.github.com>
2020-06-16 20:37:44 +0530

    Update routing.py

--- eduHub/routing.py
+++ eduHub/routing.py
@@ -1,15 +1,14 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from material.api.ws.rouing import websocket_urlpatterns as materials_ws_urls
-# from channels.staticfiles import StaticFilesConsumer
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
 
 application = ProtocolTypeRouter({
     # Empty for now (http->django views is added by default)
     # 'http.request': StaticFilesConsumer(),
-    'websocket': AuthMiddlewareStack(
+    'websocket': AuthMiddlewareStack(  #Author middleware stack for authentication
         URLRouter(
-            materials_ws_urls
+            materials_ws_urls  # deals with all asgi/web socket request related to materials app
         )
     ),
 })
