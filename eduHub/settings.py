ee00ace9051bbee41cf0fc6a0d4f0db4a5158153
aditya <kumar.93@iitj.ac.in>
2020-06-16 16:03:27 +0530

    modified settings for local server

--- eduHub/settings.py
+++ eduHub/settings.py
@@ -81,20 +81,20 @@ ASGI_APPLICATION = "eduHub.routing.application"
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 # Production database settings
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'eduHub',
-    }
-}
-
-# Default sqllite database settings
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'eduHub',
 #     }
 # }
+
+# Default sqllite database settings
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
