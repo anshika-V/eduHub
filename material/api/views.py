377a80d00f2e229fed338bd84f78b071ec8bd5d3
aditya <kumar.93@iitj.ac.in>
2020-06-17 16:08:07 +0530

    material api refactored

--- material/api/views.py
+++ material/api/views.py
@@ -10,7 +10,7 @@ import json
 
 @csrf_exempt
 @login_required
-def NewTest(request):
+def NewTest(request):  # form for creating new test, this is csrf safe af i am sending csrf token can remove @csrf_exempt in production
     if (request.method == 'POST'):
         test = Test(instructor=request.user,
                     title=request.POST['title'], description=request.POST['description'])
@@ -22,7 +22,7 @@ def NewTest(request):
     return render(request, 'material/csrf_token.html')
 
 
-@login_required
+@login_required  # Provides all data of the instructor dashboard add other data as the projects goes on
 def InstructorDashboardData(request):
     user = request.user
     testsObj = user.test_set.all()
@@ -40,14 +40,14 @@ def InstructorDashboardData(request):
 
 @login_required
 @allow_instructor
-def AllTestData(request, key):
+def AllTestData(request, key):  # Returns all data releated to a particular test
     user = request.user
     test = None
     try:
         test = Test.objects.get(pk=int(key))
     except:
         return HttpResponseNotFound('Error: test not found')
-    if (test.instructor != user):
+    if (test.instructor != user):  # Checking if requesting user is the instructor of the requested test
         return HttpResponseForbidden('Unauthorised Access: You are not the instructor of requested test')
     json_test_data = serializers.serialize(
         'json', [test],  use_natural_foreign_keys=True)
@@ -55,16 +55,16 @@ def AllTestData(request, key):
     questions = test.question_set.all()
     question_data = serializers.serialize('json', questions)
     question_data = json.loads(question_data)
+    # Adding all theh questions of the test in the response
     json_test_data['questions'] = question_data
     response_data = json.dumps(json_test_data)
     return HttpResponse(response_data, content_type='json_comment_filtered')
 
 
 @login_required
-@csrf_exempt
-def SaveTestResponse(request, key):
+@csrf_exempt  # Thsi is possibily not csrf safe, make it safe in upcoming iteration
+def SaveTestResponse(request, key):  # Save the response of a student who submitted a test
     if request.method == 'POST':
-        print(request.POST)
         user = request.user
         data = request.POST['response']
         res = TestResult(
@@ -76,8 +76,10 @@ def SaveTestResponse(request, key):
 
 
 @login_required
-def TestResponses(request, key):
+def TestResponses(request, key):  # Summary data of a All The test response of a particular test
     test = Test.objects.get(pk=key)
+    if (test.instructor != user):  # Checking if requesting user is the instructor of the requested test
+        return HttpResponseForbidden('Unauthorised Access: You are not the instructor of requested test')
     res = test.testresult_set.all()
     data = serializers.serialize(
         'json', res, fields=['student', 'time'], use_natural_foreign_keys=True)
@@ -85,15 +87,19 @@ def TestResponses(request, key):
 
 
 @login_required
-def TestResponseData(request, key):
-    data = TestResult.objects.get(pk=key)
-    data = serializers.serialize('json', [data])
+def TestResponseData(request, key):  # Test response data of a particular test response
+    res = TestResult.objects.get(pk=key)
+    test = res.parent_test
+    # Checking if requesting user is the instructor of the requested test result test
+    if (test.instructor != user):
+        return HttpResponseForbidden('Unauthorised Access: You are not the instructor of requested test')
+    data = serializers.serialize('json', [res])
     return HttpResponse(data, content_type='json_comment_filtered')
 
 
-@csrf_exempt
+@csrf_exempt  # Possibily Not secure make it in next iteration
 @login_required
-@allow_instructor
+@allow_instructor   # For saving the updated test response link marks and remarks
 def CheckSaveTestResult(request, key):
     res = TestResult.objects.get(pk=key)
     res.questions = request.POST['data']
@@ -102,10 +108,10 @@ def CheckSaveTestResult(request, key):
     return HttpResponse('Saved')
 
 
-@csrf_exempt
+@csrf_exempt  # Possibily Not secure make it in next iteration
 @login_required
 @allow_instructor
-def DeleteTest(request, key=None):
+def DeleteTest(request, key=None):  # for deleting a test
     if request.method == 'POST':
         test = Test.objects.get(pk=key)
         test.delete()
--- material/urls.py
+++ material/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('api/', include('material.api.urls')),
-    path('create-test/<int:key>/', views.CreateTest),
+    path('create-test/<int:key>/', views.CreateTest, name="create-test"),
     path('instructor-portal/', views.InstructorPortal, name='instructor-portal'),
-    path('student-test/<int:key>/', views.StudentTest),
-    path('check-test/<int:key>/', views.CheckTest)
+    path('student-test/<int:key>/', views.StudentTest, name="student-test"),
+    path('check-test/<int:key>/', views.CheckTest, name="check-test")
 ]
