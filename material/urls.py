c174ef484df3df2bb7b97b34fffcf1be1e45054a
aditya <kumar.93@iitj.ac.in>
2020-06-17 08:44:26 +0530

    url name fix

--- material/urls.py
+++ material/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns = [
     path('api/', include('material.api.urls')),
     path('create-test/<int:key>/', views.CreateTest),
-    path('instructor-portal/', views.InstructorPortal),
+    path('instructor-portal/', views.InstructorPortal, name='instructor-portal'),
     path('student-test/<int:key>/', views.StudentTest),
     path('check-test/<int:key>/', views.CheckTest)
 ]
